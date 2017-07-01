cbe87012331d12d14ddfd1f3068b618b09b30a7a
Kynosaur <kye.bracey@gmail.com>
2017-07-01 14:47:56 +0100

    Adds functionality for making posts (with a caption only) and viewing them in the index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_url
+  end
+
+end
+
+def post_params
+  params.require(:post).permit(:caption)
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   it 'should get index' do
-    get posts_url
+    get :index
     assert_response :success
   end
 
   it 'should create a new post' do
-    post posts_url, params: { post: { caption: "Hello test!" } }
-    assert redirected_to posts_path
+    post :create, params: { post: { caption: "Hello test!" } }
+    assert redirect_to posts_path
   end
 end
