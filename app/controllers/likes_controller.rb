afee56f6e30f5f00d437fca6b3fa21691bdcc94f
Kynosaur <kye.bracey@gmail.com>
2017-07-03 08:19:18 +0100

    Posts can now be liked

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,9 @@
 class LikesController < ApplicationController
+  def create
+    post = Post.find_by(id: params[:post_id])
+    like = post.likes.build
+    like.user = current_user
+    like.save
+    redirect_to posts_path
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   mount_uploader :picture, PictureUploader
 
   has_many :comments
+  has_many :likes
 
   belongs_to :user
 
