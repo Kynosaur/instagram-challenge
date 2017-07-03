46a29208c6f653c92e642c41bb8972e05680dd40
Kynosaur <kye.bracey@gmail.com>
2017-07-03 08:30:32 +0100

    Users can now like each post only once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,11 @@
 class LikesController < ApplicationController
   def create
     post = Post.find_by(id: params[:post_id])
-    like = post.likes.build
-    like.user = current_user
-    like.save
+    if current_user && !post.likes.any? { |like| like.user == current_user }
+      like = post.likes.build
+      like.user = current_user
+      like.save
+    end
     redirect_to posts_path
   end
 end
