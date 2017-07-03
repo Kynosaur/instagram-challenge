b005375369a97a7ab3252142fe1378568d4be377
Kynosaur <kye.bracey@gmail.com>
2017-07-03 08:28:00 +0100

    Adds tests for users only being able to like each post once

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -7,4 +7,19 @@ RSpec.feature 'Likes', type: :feature do
     click_button('Like')
     expect(page).to have_content('Likes: 1')
   end
+
+  scenario 'Like button only appears if user is logged in' do
+    sign_up('test@test.com', 'password', 'test user')
+    create_new_post('Hello test!')
+    expect(page).to have_button('Like')
+    click_link('Log out')
+    expect(page).not_to have_button('Like')
+  end
+
+  scenario 'Users can only like each post once' do
+    sign_up('test@test.com', 'password', 'test user')
+    create_new_post('Hello test!')
+    click_button('Like')
+    expect(page).not_to have_button('Like')
+  end
 end
